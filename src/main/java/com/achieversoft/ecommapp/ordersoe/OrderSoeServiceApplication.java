59384eecd10d2c01ab5734a848b6c4950117cf67
prsgithub <ranjansahoo.prakash@gmail.com>
2025-03-14 00:18:26 +0530

    1st commit

---
+++ src/main/java/com/achieversoft/ecommapp/ordersoe/OrderSoeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.achieversoft.ecommapp.ordersoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSoeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSoeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/achieversoft/ecommapp/ordersoe/OrderSoeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achieversoft.ecommapp.ordersoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSoeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
